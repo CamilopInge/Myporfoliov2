cac744d831b6d6cc0de86c911c0345546a1c7678
Camilo Aguirre <brayan93j.k@gmail.com>
2025-06-01 12:45:55 -0500

    feat: agregar toggle de tema oscuro con Tailwind

---
+++ src/locales/en.ts
@@ -0,0 +1,27 @@
+export default {
+    header: {
+        greeting: "Hi! I'm Camilo",
+        role: "Student System Engineer",
+        description: "I develop fluid, efficient and easy-to-use digital experiences, handling both the front-end but I like the back-end more.",
+        nav: {
+            about: "About",
+            experience: "Experience",
+            projects: "Projects",
+            skills: "Skills",
+            certificates: "Certificates",
+            cv: "Check CV"
+        }
+    },
+    sections: {
+        about: {
+            title: "About me",
+            p1: "I started studying systems engineering in 2021, creating web designs. I didn't exploit my creativity much but I got along quite well within the code, creating functionalities in the Back-end. I haven't had the privilege of working with a company, but I support myself with projects to solve problems.",
+            p2: "My main current goal is to continue working on experience and error management. What I like most is creating scalable, aesthetic and effective software, that looks well designed but works the right way. In my free time I upload videos to YouTube about Linux (Servers, Installations, Console)",
+            p3: "When I'm not in front of the computer, I like to study, play, read and exercise. I usually think about how to improve every day and I even put code in my life to help me make things easier (I could do it in 1 minute but I decided to automate it), currently I'm looking to develop what I'm doing."
+        },
+        experience: "Experience",
+        projects: "Projects",
+        skills: "Skills",
+        certificates: "Certificates"
+    }
+};
---
+++ src/locales/es.ts
@@ -0,0 +1,27 @@
+export default {
+  header: {
+    greeting: "¡Hola! Soy Camilo",
+    role: "Estudiante de Ingeniería de Sistemas",
+    description: "Desarrollo experiencias digitales fluidas, eficientes y fáciles de usar...",
+    nav: {
+      about: "Sobre mí",
+      experience: "Experiencia",
+      projects: "Proyectos",
+      skills: "Habilidades",
+      certificates: "Certificados",
+      cv: "Ver CV"
+    }
+  },
+  sections: {
+    about: {
+      title: "Sobre mí",
+      p1: "Empecé a estudiar ingeniería de sistemas en 2021...",
+      p2: "Mi objetivo principal actualmente es seguir...",
+      p3: "Cuando no estoy frente al computador..."
+    },
+    experience: "Experiencia",
+    projects: "Proyectos",
+    skills: "Habilidades",
+    certificates: "Certificados"
+  }
+};
